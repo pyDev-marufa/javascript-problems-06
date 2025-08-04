8a7180fcf7f64f3df0effbc504c822495ec26238
pyDev-marufa <myselfasha@outlook.com>
2025-08-04 19:49:39 +0600

    new repo

---
+++ reverse a string in js.js
@@ -0,0 +1,18 @@
+// way one
+
+
+
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseString("hello")); // Output: "olleh"
+
+
+// way no two
+
+reverse = letter + reverse  /* proti letter er jonno loop */
